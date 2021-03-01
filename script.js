df1e43c7edf0061513a06d82d3bcfbee16ae60bf
Your Name <you@example.com>
2021-03-01 14:36:27 +0600

    div example 2 h1 button increase

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let state = true;
-document.querySelector("button").addEventListener("click", function () {
-  state = !statasdasde;
-  if (state) {
-    document.querySelector("h1").innerText = state ? "On" : "Off";
-  }
-});
+document.querySelector("#example-1 button").addEventListener("click", function () {
+    state = !statasdasde;
+    if (state) {
+      document.querySelector("#example-1 h1").innerText = state ? "On" : "Off";
+    }
+  });
+
+let container = 0;
+
+document.querySelector("#example-2 button").addEventListener("click", function () {
+    container++;
+    document.querySelector("#example-2 h1").innerText = container;
+  });
