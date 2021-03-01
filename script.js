e0ed693f22cb4c1019323fc20411026c635684c9
Your Name <you@example.com>
2021-03-01 14:47:41 +0600

    document query selector input

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ document.querySelector("#example-2 button").addEventListener("click", function (
     container++;
     document.querySelector("#example-2 h1").innerText = container;
   });
+
+ let container2 = "";
+
+document.querySelector("#example-3 input").addEventListener("input", function () {
+    container2 = this.value.trim();
+    document.querySelector("#example-3 h1").innerText = "Your name is";
+  });
+
+
